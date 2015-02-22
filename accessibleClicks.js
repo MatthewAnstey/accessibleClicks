187695f129a3447931876a634126afe48b000890
= <mtthwnsty@gmail.com>
2015-02-22 14:16:03 +0000

    Initial file added - tested with lastest chrome and firefox - works with both Click and On methods

---
+++ accessibleClicks.js
@@ -0,0 +1,39 @@
+(function ($) {
+"use strict";
+$.fn.oldOn = jQuery.fn.on;
+$.fn.extend({
+    on:  function() {
+        var args = Array.prototype.slice.call(arguments), 
+            newArgs = args.slice(0), 
+            i = 0,
+            enterKey = 13,
+            spaceBar = 32, 
+            callBackPosition;
+        
+        $.fn.oldOn.apply(this, args) ;
+        
+        for (; i < args.length; i++) {
+            if (typeof args[i] === 'function') {
+                callBackPosition = i;
+                break;
+            }
+        }
+       
+        if (args[0] === 'click') {    
+            newArgs[0] = 'keydown'; 
+            newArgs[callBackPosition] = function (evt) {
+                if (evt.which === enterKey || 
+                    evt.keyCode === enterKey || 
+                    evt.which === spaceBar ||
+                    evt.keyCode === spaceBar) {
+                    args[callBackPosition]();
+                }
+            };
+            $.fn.oldOn.apply(this, newArgs);
+        }
+    }
+   
+});
+    
+   
+}(jQuery));
