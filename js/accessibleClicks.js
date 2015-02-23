d8644fe0152a35606b08cf54a28f91873b467e1c
= <mtthwnsty@gmail.com>
2015-02-23 21:15:47 +0000

    added support for off, for specific functions

--- js/accessibleClicks.js
+++ js/accessibleClicks.js
@@ -12,6 +12,9 @@
                 spaceBar = 32,
                 callBackPosition;
 
+                $.fn.oldOnOriginalCallbacks = $.fn.oldOnOriginalCallbacks ? 
+                    $.fn.oldOnOriginalCallbacks : {};
+
             $.fn.oldOn.apply(this, args);
 
             for (; i < args.length; i++) {
@@ -21,7 +24,8 @@
                 }
             }
 
-            if (args[0] === 'click') {
+            if (args[0] === 'click' && 
+                typeof callBackPosition !== 'undefined') {
                 newArgs[0] = 'keydown';
                 newArgs[callBackPosition] = function(evt) {
                     if (evt.which === enterKey ||
@@ -31,15 +35,33 @@
                         args[callBackPosition](evt);
                     }
                 };
+
+                $.fn.oldOnOriginalCallbacks[newArgs[callBackPosition].toString()] = 
+                    args[callBackPosition];
+
                 $.fn.oldOn.apply(this, newArgs);
             }
         },
 
         off: function () {
             var args = Array.prototype.slice.call(arguments),
-                newArgs = args.slice(0);
+                newArgs = args.slice(0), 
+                callBackPosition,
+                i = 0;
 
                 $.fn.oldOff.apply(this, args);
+
+                for (; i < args.length; i++) {
+                    if (typeof args[i] === 'function') {
+                        callBackPosition = i;
+                        break;
+                    }
+                }
+
+                if (typeof callBackPosition !== 'undefined') {
+                    newArgs[callBackPosition] =  
+                        $.fn.oldOnOriginalCallbacks[args[callBackPosition].toString()];
+                }
 
                 if (args[0] === 'click') {
                     newArgs[0] = 'keydown';
